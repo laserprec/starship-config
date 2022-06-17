b687a308d03c4ef5f7857b499335fff411b41afc
Jianjie Liu <jianjl@microsoft.com>
2022-06-17 09:34:18 -0400

    add starship config

---
+++ install-starship.sh
@@ -0,0 +1,11 @@
+profile_file=~/.bashrc
+starship_home=~/.starship
+
+curl -sS https://starship.rs/install.sh | sh
+
+mkdir -p $starship_home
+cp ./starship.toml ~/.starship
+echo ">>>> Setting up starship in '$profile_file'"
+echo "### starship config ###" >> $profile_file
+echo "export STARSHIP_CONFIG=~/.starship/starship.toml" >> $profile_file
+echo 'eval "$(starship init bash)"' >> $profile_file
